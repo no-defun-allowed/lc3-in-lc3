2837f14d437923b1ddac7d23bdaa4502801c8f0e
Hayley Patton <theemacsshibe@gmail.com>
2021-11-13 21:23:38 +1100

    Export BUILD-VM

--- macro-assembler/package.lisp
+++ macro-assembler/package.lisp
@@ -1,3 +1,4 @@
 (defpackage :lc3
   (:use :cl)
-  (:shadow #:not #:and #:return #:ldb))
+  (:shadow #:not #:and #:return #:ldb)
+  (:export #:build-vm))
---
+++ vm/lc3-in-lc3.asd
@@ -0,0 +1,3 @@
+(asdf:defsystem :lc3-in-lc3
+  :depends-on (:lc3-macro-assembler)
+  :components ((:file "build-vm")))
